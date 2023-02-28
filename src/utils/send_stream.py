b691da6a0d1268ebb59be6641152062bc9293b75
tspy <tspy@milestone.dk>
2023-02-28 12:40:34 +0100

    refine stream service

--- src/utils/send_stream.py
+++ src/utils/send_stream.py
@@ -1,23 +1,22 @@
-from kafka import KafkaProducer
 import json
-
+import time
+import numpy as np
+from kafka import KafkaProducer
 from utilities import load_data
 
 # Load CIFAR10 data
 trainloader, testloader = load_data()
 
 # Define Kafka producer configuration
-kafka_broker = 'localhost:9092'
-producer_config = {'bootstrap_servers': [kafka_broker]}
-
-# Create Kafka producer
-producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'), **producer_config)
+producer = KafkaProducer(bootstrap_servers='kafka-broker:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 # Send CIFAR10 data to Kafka
-for data, _ in testloader:
-    data = data.numpy()
-    message = {'data': data.tolist()}
-    print(message)
-    producer.send('cifar10-data', value=message)
+while True:
+    for data, _ in testloader:
+        time.sleep(1)
+        data = data.numpy()
+        message = {'data': data.tolist()}
+        producer.send("CIFAR-10", 
+                    'cifar10-data', value=message)
 
-producer.flush()
+    producer.flush()
