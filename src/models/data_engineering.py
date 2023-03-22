dbacc589684a44e325dbff2757def8cfca72eb7d
ThomaSpyrou <spyroutommy@gmail.com>
2023-03-22 14:57:22 +0100

    fixed issues with loaders and prediction

--- src/models/data_engineering.py
+++ src/models/data_engineering.py
@@ -3,15 +3,19 @@ import numpy as np
 import ast
 
 
-df = pd.read_csv("dataset_1.csv")
+df = pd.read_csv("dataset.csv")
 
 # df['softmax_values'] = df['softmax_values'].to_list()
 
 df['soft_list'] = df['softmax_values'].apply(lambda x: ast.literal_eval(x))
+# df['soft'] = df['soft_list'].apply(ast.literal_eval)
 
-df['soft'] = df['soft_list'].apply(lambda x: pd.Series(x).median())
+print(type(df.soft_list[0]))
+df['pred_soft'] = df['pred_labels'].apply(lambda x: (df['soft_list'][0][x]))
+df['true_soft'] = df['true_labels'].apply(lambda x: (df['soft_list'][0][x]))
+# df['soft'] = df['soft_list'].apply(lambda x: pd.Series(x).median())
 
-final_df = df[["soft", "pred_labels", "true_labels"]]
+final_df = df[["pred_labels", "true_labels", "pred_soft", 'true_soft', "soft_list"]]
 
 
 final_df.to_csv("./final_1.csv", 
---
+++ src/models/estimation.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import torch
+import ast
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+class MyDataset(Dataset):
+    def __init__(self, data_path):
+        self.data = pd.read_csv(data_path)
+        self.data['soft_list'] = self.data['soft_list'].apply(lambda x: ast.literal_eval(x))
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        soft_list = torch.tensor((self.data.iloc[idx]['soft_list']))
+        pred_soft = torch.tensor(self.data.iloc[idx]['pred_soft'])
+        return soft_list, pred_soft
+
+data_path = 'final_1.csv'
+dataset = MyDataset(data_path)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+
+class IamWatchingU(nn.Module):
+    def __init__(self):
+        super(IamWatchingU, self).__init__()
+        self.l1 = nn.Linear(10, 100)
+        self.l2 = nn.Linear(100, 200)
+        self.dropout = nn.Dropout(p=0.2)
+        self.output = nn.Linear(200, 1)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.l1(x)
+        x = self.relu(x)
+        x = self.l2(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.output(x)
+        x = self.sigmoid(x)
+
+        return x
+
+model = IamWatchingU()
+optimizer = torch.optim.Adam(model.parameters())
+criterion = nn.MSELoss() 
+
+def train_model(model, dataloader, optimizer, criterion, num_epochs=10):
+    device = torch.device('cpu' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+    
+    for epoch in range(num_epochs):
+        total_loss = 0.0
+        for i, (inputs, targets) in enumerate(dataloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs.float())
+            loss = criterion(outputs.squeeze(), targets.float())
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+            predicted_values = outputs.detach().numpy()
+
+            print((predicted_values))
+            
+        print(f"Epoch {epoch+1}/{num_epochs}, Loss: {total_loss / len(dataloader):.4f}")
+        
+
+train_model(model, dataloader, optimizer, criterion)
