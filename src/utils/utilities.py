8fa4f7289b4646fb06a2e6b3a680c30fba8c57b0
tspy <tspy@milestone.dk>
2023-04-26 15:40:57 +0200

    Revert to cifar10

--- src/utils/utilities.py
+++ src/utils/utilities.py
@@ -113,8 +113,8 @@ def load_data():
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
-    trainset = torchvision.datasets.CIFAR100(root='../../data/processed', train=True, download=True, transform=transform_train)
-    testset = torchvision.datasets.CIFAR100(root='../../data/processed', train=False, download=True, transform=transform_test)
+    trainset = torchvision.datasets.CIFAR10(root='../../data/processed', train=True, download=True, transform=transform_train)
+    testset = torchvision.datasets.CIFAR10(root='../../data/processed', train=False, download=True, transform=transform_test)
 
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
     testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
