384b235ceb4d2785152f663e1b540298df7f5074
tspy <tspy@milestone.dk>
2023-05-04 11:04:08 +0200

    "new idea for training fixing dataset"

--- src/models/data_engineering.py
+++ src/models/data_engineering.py
@@ -4,16 +4,18 @@ import argparse
 
 
 def clean_target_output():
-    true_labels = pd.read_csv('true_labels100.txt', header=None, names=['true_label'])
-    predictions = pd.read_csv('predicted_labels100.txt', header = None, names=['prediction'])
+    true_labels = pd.read_csv('/home/tspy/workspace/thesis/ThesisDTU/data/estimation_generated/true_labels100.txt', header=None, names=['true_label'])
+    predictions = pd.read_csv('/home/tspy/workspace/thesis/ThesisDTU/data/estimation_generated/predicted_labels100.txt', header = None, names=['prediction'])
 
     # softmax value needs thalpori
     cols = [i for i in range(100)]
-    softmax_values = pd.read_csv('softmax_values100.txt', header=None, sep='\s+', names=cols)
+    softmax_values = pd.read_csv('/home/tspy/workspace/thesis/ThesisDTU/data/estimation_generated/softmax_values100.txt', header=None, sep='\s+', names=cols)
     for index in range(100):
         softmax_values[index] = softmax_values[index].str.extract('(\d+\.\d+)').astype(float)
 
     softmax_values['soft_list'] = softmax_values.values.tolist()
+    softmax_values['soft_list'] = softmax_values['soft_list'].apply(lambda x: [round(i, 2) for i in x])
+    softmax_values['soft_list'] = softmax_values['soft_list'].apply(lambda x: [0.5 if i > 1 or i < 0 else i for i in x])
     softmax_values = softmax_values.drop(cols, axis=1)
 
     tmp = pd.concat([true_labels, predictions], axis=1, ignore_index=True)
@@ -24,7 +26,7 @@ def clean_target_output():
     df['true_soft'] = df.apply(take_correct_value_true, axis=1)
 
     final_df = df[["pred", "true", "pred_soft", 'true_soft', "soft"]]
-    final_df.to_csv("./final.csv", index=False)
+    final_df.to_csv("./final100.csv", index=False)
 
 
 def modify_soft(row):
