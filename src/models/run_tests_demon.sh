58b3bc8ed8aeafc9d0b66e39996ba65d996ca92b
tspy <tspy@milestone.dk>
2023-06-13 09:48:36 +0200

    .

--- src/models/run_tests_demon.sh
+++ src/models/run_tests_demon.sh
@@ -2,7 +2,7 @@
 
 # Define the range of indices for the loop
 start_index=0
-end_index=9
+end_index=10
 for ((index=start_index; index<=end_index; index++))
 do
     echo "Running script with index $index"
